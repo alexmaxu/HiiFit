f1f818d0a791a8ee4011a3714fb1e6677905c2c2
alex <alexmaxu12@gmail.com>
2023-12-20 09:48:10 +0100

    save history view

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -9,6 +9,16 @@ struct HIITFitApp: App {
     WindowGroup {
       ContentView()
             .environmentObject(historyStore)
+            .alert(isPresented: $historyStore.loadingError) {
+              Alert(
+                title: Text("History"),
+                message: Text(
+                  """
+                  Unfortunately we can't load your past history.
+                  Email support:
+                    support@xyz.com
+                  """))
+            }
     }
   }
 }
--- HIITFit/Model/HistoryStore.swift
+++ HIITFit/Model/HistoryStore.swift
@@ -9,7 +9,14 @@ struct ExerciseDay: Identifiable {
 }
 
 class HistoryStore: ObservableObject {
-  @Published var exerciseDays: [ExerciseDay] = []
+    
+    @Published var loadingError = false
+    @Published var exerciseDays: [ExerciseDay] = []
+    
+    var dataURL: URL {
+      URL.documentsDirectory
+        .appendingPathComponent("history.plist")
+    }
   
     enum FileError: Error {
         case loadFailure
@@ -25,28 +32,71 @@ class HistoryStore: ObservableObject {
     do {
         try load()
     } catch {
-        print("Error:", error)
+        loadingError = true
     }
       
   }
   
     func load() throws {
+      do {
+    // 1
+        let data = try Data(contentsOf: dataURL)
+        // 2
+        let plistData = try PropertyListSerialization.propertyList(
+          from: data,
+          options: [],
+          format: nil)
+    // 3
+        let convertedPlistData = plistData as? [[Any]] ?? []
+        // 4
+        exerciseDays = convertedPlistData.map {
+          ExerciseDay(
+            date: $0[1] as? Date ?? Date(),
+            exercises: $0[2] as? [String] ?? [])
+    }
+    } catch {
         throw FileError.loadFailure
+      }
+    }
+    
+    func save() throws {
+        let plistData = exerciseDays.map {
+          [$0.id.uuidString, $0.date, $0.exercises]
+        }
+        
+        do {
+            let data = try PropertyListSerialization.data(
+                fromPropertyList: plistData,
+                format: .binary,
+                options: .zero)
+            // 2
+              try data.write(to: dataURL, options: .atomic)
+        } catch {
+            // 3
+              throw FileError.saveFailure
+        }
+
     }
   
-  func addDoneExercise(_ exerciseName: String) {
-    let today = Date()
-      if let firstDate = exerciseDays.first?.date,
-        today.isSameDay(as: firstDate) { // 1
-      print("Adding \(exerciseName)")
-      exerciseDays[0].exercises.append(exerciseName)
-    } else {
-      exerciseDays.insert( // 2
-        ExerciseDay(date: today, exercises: [exerciseName]),
-        at: 0)
+    func addDoneExercise(_ exerciseName: String) {
+        let today = Date()
+        if let firstDate = exerciseDays.first?.date,
+            today.isSameDay(as: firstDate) { // 1
+            print("Adding \(exerciseName)")
+            exerciseDays[0].exercises.append(exerciseName)
+        } else {
+            exerciseDays.insert( // 2
+                ExerciseDay(date: today, exercises: [exerciseName]),
+                at: 0)
+        }
+        print("History: ", exerciseDays)
+        
+        do {
+          try save()
+        } catch {
+          fatalError(error.localizedDescription)
+        }
     }
-    print("History: ", exerciseDays)
-  }
 }
 
 
